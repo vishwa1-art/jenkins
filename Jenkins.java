61bb09fcbb85462f62de60db2dece9621ffcc39a
vishwa1-art <maheshwaramshweta1@gmail.com>
2024-11-29 19:17:01 +0530

    Initial commit

---
+++ Jenkins.java
@@ -0,0 +1,17 @@
+package deploy.jenkins;
+
+public class Jenkins {
+public Jenkins() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//C:\Users\mahes\Desktop\Jenkins\deploy
+		//
+		//C:\Users\mahes\Desktop\Jenkins\deploy\jenkins
+		
+		System.out.println("hello deploy");
+	}
+
+}
